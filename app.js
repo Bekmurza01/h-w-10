7e789258b7132901e015fc18762be261e351b623
Bekmurza01 <bekmurzausmanov197@gmail.com>
2024-09-26 20:36:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// console.log
+// Number
+// Boolean
+// null
+// undefined
+// const nam1 = '29'
+// const nam2 = '50'
+// const res = nam1 + nam2
+// console.log(res)
+// console.log(nam1 + nam2)
+// const nam1 = '29'
+// const nam2 = '50'
+
+// console. log  = nam1 + Number(nam2)
+// const num1 = '29'
+// const num2 = '50'
+// console.res2(num1)
+// console.res3(num2)    
+// console.log (num1 == Number(num2))
+// console.log(num2) if else
+
+// const text = prompt('rfu')
+
+// if (text >= 12) {
+//     console.log('жаман')
+// } else if (text ==3 ) {
+//     //  console.log('bul 0')
+// } else {
+//     console.log('начар')
+// }   if (text ==4) {
+//     console.log('норм')
+// } else (text == 5 ) 
+//     console.log('эн жакшы')
+
+// const time = parseInt(prompt('dfghj'))
+
+//     if (time >= 12){
+//         console.log('доброе утро')
+//     } else if (time >= 22){
+//         console.log('спокойный ноч')
+//     }
+
+    // const checkage = function(num) {
+    // if (time < 12){
+    //     console.log(`baby`);
+    // } if  (time < 18) {
+    //  console.log(`teenager`);
+    // } if (time < 60); {
+    //     console.log(`old`);
+    // }
+    // }
+
+    // checkage (15)   
